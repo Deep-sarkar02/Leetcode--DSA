bb8b930605f51ea1fb6076094c580f796f2f00cf
Chndrangshu sarkar <121252899+Deep-sarkar02@users.noreply.github.com>
2025-08-07 10:05:36 +0530

    Time: 45 ms (43.15%), Space: 44.2 MB (77.39%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,40 +1,38 @@
 class Solution {
-    public int lowerbound(List<Integer> subset , int target)
-    {
-        int ans = subset.size();
-        int left = 0;
-        int right = subset.size()-1;
-        while(left <= right)
+    public int lengthOfLIS(int[] nums) {
+        // create the array to memoise
+        int [] t = new int [nums.length];
+        // loop over the array
+        // i at 1
+        // j at 0
+        for(int i = 1 ; i<nums.length ; i++)
         {
-            int mid = (left + right)/2;
-            if(subset.get(mid) >= target)
+            // i always atart at 1
+            // now j will staart from 0 to i
+            for(int j = 0 ; j< i ; j++)
             {
-                ans =  mid;
-                right = mid -1;
-            }
-            else
-            {
-                left = mid+1;
+                // if the j value wll be less
+                if(nums[j] < nums[i])
+                {
+                    // we will upate the  i th place in the array
+                    if(t[j] + 1 > t[i]) // if t[i] +1 will be greater than the t[j]
+                    {
+                        t[i] = t[j]+1;
+                    }
+                }
             }
         }
-        return ans;
-    }
-    public int lengthOfLIS(int[] nums) 
-    {
-        List<Integer> subset  = new ArrayList<>();
-        for (int idx = 0 ; idx < nums.length ; idx ++)
+        
+        // find the max index
+        int max = 0;
+        // loop from the t
+        for(int i = 0 ; i <t.length ; i++)
         {
-            int lb = lowerbound(subset , nums[idx]);
-            if(lb < subset.size())
+            if(t[i] > t[max])
             {
-                subset.set(lb , nums[idx]);
+                max = i; // get the max index
             }
-            else
-            {
-                subset.add(nums[idx]);
-            }
-            
-        }   
-        return subset.size();
+        }
+        return t[max] +1;
     }
 }
