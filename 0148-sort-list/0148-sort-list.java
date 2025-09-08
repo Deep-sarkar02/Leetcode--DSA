7cdc1eee66a9d92746629944e5665c462a012137
Chndrangshu sarkar <121252899+Deep-sarkar02@users.noreply.github.com>
2025-09-08 19:42:18 +0530

    Time: 15 ms (9.96%), Space: 58.6 MB (5.56%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,38 +1,66 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
+    public  ListNode findmid(ListNode head)
+    {
+        ListNode f = head;
+        ListNode s = head;
+        while(f.next != null && f.next.next != null)
+        {
+            s= s.next;
+            f = f.next.next;
+        }
+        return s;
+    }
+
+    // merge 2 sorted ll
+    public ListNode merge2sortedll(ListNode h1 , ListNode h2)
+    {
+        if(h1 == null)
+        {
+            return h2;
+            
+        }
+        if(h2 == null)
+        {
+            return h1;
+        }
+        
+        // case1
+        if(h1.val <= h2.val)
+        {
+            h1.next = merge2sortedll(h1.next , h2);
+            return h1;
+        }
+        
+        // case 2
+        else
+        {
+            h2.next = merge2sortedll(h1 , h2.next);
+            return h2;
+        }
+    }
+
     public ListNode sortList(ListNode head) {
-        //store all the element in the array
-        ArrayList<Integer> a = new ArrayList<>();
-        //loop over the ll and store all the value 
-        ListNode temp = head;
-        while(temp != null)
+        // code here
+        if(head == null || head.next == null)
         {
-            a.add(temp.val);
-            temp = temp.next;
+            return head;
         }
-        System.out.println(a);
-        Collections.sort(a);
-        // now we will loop over the array and replace 
-        int i = 0;
-        temp = head;
-        while(temp != null)
-        {
-           
-                temp.val = a.get(i);
-                i++;
-                temp = temp.next;
-            
-            
-        }
-        return head;
+        // find the middle fo the node
+        ListNode middle = findmid(head);
+        
+        // now the lefthead is the head
+        ListNode lh = head;
+        // now the right head is the left.next
+        ListNode rh = middle.next;
+        
+        // now we will make the mid.next = null
+        middle.next = null;
+        // again do this for the lh and the rh
+        ListNode sl = sortList(lh);
+        ListNode rl = sortList(rh);
+        
+        // merege 2 sorted ll
+        return merge2sortedll(sl , rl);
     }
 }
